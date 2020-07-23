aae5ca7bd5a7a7f759e81a3ce58583dad4e9c30a
Brian Ho <yobrianh@gmail.com>
2020-07-23 22:29:24 +0000

    Added comments to contact form/ controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,24 +1,35 @@
 class ContactsController < ApplicationController
+    
+    #GET request to /contact-us
+    #Show new contact form 
 def new
     @contact = Contact.new
 end
 
+    #POST request /contacts
 def create
+    #Assign form fields to Contact object
     @contact = Contact.new(contact_params)
+    #Save Contact to db
     if @contact.save
         name = params[:contact][:name]
         email = params[:contact][:email]
         body = params[:contact][:comments]
+        #Plug variables to email and send
         ContactMailer.contact_email(name,email,body).deliver
+        #Give success method and go to new action
         flash[:success] = "Message sent"
        redirect_to new_contact_path
     else
+        #Error handling
         flash[:danger] = @contact.errors.full_messages.join(", ")
        redirect_to new_contact_path
     end
 end
 
 private
+    # Strong params required to collect data from from.
+    # Rails security requirement
   def contact_params
      params.require(:contact).permit(:name, :email, :comments)
   end  
